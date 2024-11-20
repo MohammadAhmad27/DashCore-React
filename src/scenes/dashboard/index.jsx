878880efec7808a4ca1ca449c7f895ecd409a8c3
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-11-20 22:08:13 +0500

    Added <Dashboard/> <Header /> & <Team /> components and updated import in mockData.jsx

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header'
+import { Box } from '@mui/material'
 
 export default function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="DASHBOARD" subTitle="Welcome to your Dashboard" />
+            </Box>
+        </Box>
     )
 }
